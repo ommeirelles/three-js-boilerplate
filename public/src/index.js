76a20c5841f20a2d09e581c0b951aef88b7eb63d
Otávio <ommeirelles@gmail.com>
2022-01-12 16:37:34 -0300

    adding support to model load

--- public/src/index.js
+++ public/src/index.js
@@ -1,60 +1,54 @@
-import * as  THREE from 'three'
+import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { Camera } from './camera'
+import { Helmet } from './helmet'
 import { Renderer } from './renderer'
 
 const clock = new THREE.Clock()
 const config = {
-	screenSize: {
-		width: window.innerWidth,
-		height: window.innerHeight
-	},
-	maxPixelRatio: 2,
-	scene: new THREE.Scene(),
-	camera: undefined,
-	renderer: undefined
+    screenSize: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+    },
+    maxPixelRatio: 2,
+    scene: new THREE.Scene(),
+    camera: undefined,
+    renderer: undefined,
 }
 
 config.camera = new Camera(config)
 config.renderer = new Renderer(config)
 
-const cube = new THREE.Mesh(
-	new THREE.BoxGeometry(1, 1, 1),
-	new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+const controls = new OrbitControls(
+    config.camera.camera,
+    config.renderer.renderer.domElement
 )
-cube.material.wireframe = true;
-config.scene.add(cube)
-
-const controls = new OrbitControls(config.camera.camera, config.renderer.renderer.domElement);
 controls.enableDamping = true
 
-let windowResized = false;
+let windowResized = false
 window.addEventListener('resize', () => {
-	config.screenSize.width = window.innerWidth
-	config.screenSize.height = window.innerHeight
-	windowResized = true;
+    config.screenSize.width = window.innerWidth
+    config.screenSize.height = window.innerHeight
+    windowResized = true
 })
 
-let delta = 0;
+new Helmet(config.scene)
+const light = new THREE.AmbientLight(0x404040) // soft white light
+config.scene.add(light)
+
+let delta = 0
 const loop = function () {
-	delta = clock.getDelta();
+    delta = clock.getDelta()
 
-	requestAnimationFrame(loop)
-	controls.update();
-	config.renderer.update();
+    requestAnimationFrame(loop)
+    controls.update()
+    config.renderer.update()
 
-	if (windowResized) {
-		config.camera.resized();
-		config.renderer.resized();
-	}
-	windowResized = false;
-
-
-	((d) => {
-		cube.rotation.x += 1 * d
-		cube.rotation.y += 1 * d
-		cube.rotation.z += 1 * d
-	})(delta)
+    if (windowResized) {
+        config.camera.resized()
+        config.renderer.resized()
+    }
+    windowResized = false
 }
 
 document.body.appendChild(config.renderer.renderer.domElement)
